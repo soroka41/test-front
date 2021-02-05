c83025480757b23da6bcf4d9eb305d00067f2508
unknown <support@de2com.com>
2021-02-05 14:43:07 +0300

    add test front

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,13 +1,72 @@
 <template>
-  $END$
+  <div class="card mt-3 mb-3">
+    <div class="card-body"   v-if="card">
+      <p class="card-text" v-if="!showRedact">{{card.text_commit}}</p>
+      <div class="form-group" v-if="showRedact">
+        <label for="">Отредактировать коммит</label>
+        <input type="text" class="form-control" placeholder="Написать ваш комментарий" v-model="card.text_commit">
+      </div>
+      <div class="wrap-block-comment">
+        <p>{{card.user_commit}}</p>
+        <p>{{card.date_commit}}</p>
+        <div class="block-card-action">
+          <a @click.prevent="resetCommit" href="" class="w-100 btn btn-primary btn-sm" v-if="showRedact">Отмена</a>
+          <a @click.prevent="updateCommit" href="" class="w-100 btn btn-sm" :class="[!showRedact? 'btn-primary': 'btn-warning']">{{!showRedact?'Редактировать': 'Сохранить'}}</a>
+          <a @click.prevent="deleteCommit(cardIdx)" href="" class="w-100 btn btn-danger btn-sm" v-if="!showRedact">Удалить</a>
+        </div>
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
-    name: 'card.vue'
+    props: ['card', 'cardIdx'],
+    name: 'card.vue',
+    data: () => ({
+      showRedact: false,
+      commitInput: ''
+    }),
+    computed: {
+      valueInput() {
+        if(this.card.text_commit) {
+          return this.commitInput = this.card.text_commit
+        } else {
+          return this.commitInput = ''
+        }
+      }
+    },
+    methods: {
+      resetCommit() {
+        this.showRedact =! this.showRedact
+      },
+      deleteCommit(cardIdx) {
+        this.$emit('deleteCommit', cardIdx)
+
+      },
+      updateCommit() {
+        console.log(this.card)
+        this.showRedact =! this.showRedact
+        // this.$emit('updateCommit', data)
+        console.log('2', this.showRedact )
+      },
+    }
   }
 </script>
-
-<style scoped>
-
+<style lang="scss">
+  .block-card-action {
+    display: flex;
+    flex-direction: column;
+    width: 150px;
+    * {
+      margin: 2px;
+    }
+  }
+  .wrap-block-comment {
+    margin-top: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="form-group">
+            <h5 class="card-title mb-3">Продукт A</h5>
+            <input type="text" class="form-control" placeholder="Написать ваш комментарий" v-model="commitInput">
+            <div class="action-blocks" v-if="commitInput !== ''">
+              <button @click.prevent="addCommit"  class="btn btn-primary m-3">Сохранить</button>
+              <button @click.prevent="updateCommit"  class="btn btn-light">Отмена</button>
+            </div>
+            <div v-if="alert" class="alert alert-primary" role="alert">
+              Поле не должны быть пустым
+            </div>
+          </div>
+          <div v-if="commit.length">
+            <card
+              v-for="(card, idx) in commit"
+              :key="idx"
+              :cardIdx="idx"
+              :card="card"
+              @deleteCommit="deleteCommit"
+              @updateCommit="updateCommit"
+            ></card>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import card from '@/components/card.vue'
 export default {
-  name: 'Home',
+  data: () =>({
+    alert: false,
+    commit: [],
+    commitInput: ''
+  }),
   components: {
-    HelloWorld
+    card
+  },
+  methods: {
+    deleteCommit(data) {
+      let success = confirm("Удалить")
+      if(success)
+        this.commit = this.commit.filter((i, idx) => idx !== data)
+    },
+    updateCommit() {
+    },
+    addCommit() {
+      this.alert = false
+      if(this.commitInput !== '') {
+        let commitItem = {}
+        commitItem.date_commit = new Date().toLocaleString()
+        commitItem.text_commit = this.commitInput
+        commitItem.user_commit = 'Петя'
+        this.commit.unshift(commitItem)
+        this.commitInput = ''
+        console.log(this.commit)
+      } else {
+        this.alert = true
+      }
+    }
   }
 }
 </script>
+<style lang="scss">
+  .home {
+    margin: 0 auto;
+    width: 700px;
+    padding: 40px;
+  }
+</style>
